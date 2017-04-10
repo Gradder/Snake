0636fd4f7e274fa8b1dde91bfc82e820be961330
Gradder <rzhitkov@gmail.com>
2017-04-10 22:14:46 +0300

    Translating comments
    Translated comments from Russian to English

--- game.cpp
+++ game.cpp
@@ -6,18 +6,19 @@
 #include <iostream>
 #include <string>
 using namespace std;
-// Константы для указания направления перемещения змейки
+// Constants for indicating the direction of displacement of the snake
 #define DIR_RIGHT 1
 #define DIR_LEFT -1
 #define DIR_UP    2
 #define DIR_DOWN -2
-char field[50][50]; //размер поля, в котором строится игра
-int co_x[50], co_y[50], foodx, foody; //координаты змейки и еды
-const int height = 16, width = 46; // высота игрового поля и ширина
-int size = 2; //длина змейки
-int direction=1;//направление движения. Изначально движется вправо 
-int score = 0;// подсчет очков
- // Функция для установки курсора в указанную позицию
+char field[50][50]; //The size of the field where the game is built
+int co_x[50], co_y[50], foodx, foody; //Coordinates of the snake and food
+const int height = 16, width = 46; // Height of the game field and width
+int size = 2; //Length of snake
+int direction=1;//Direction of movement. Initially moves to the right
+int score = 0;// Scoring
+
+// Function for setting the cursor to the specified position
 void gotoXY (int column, int line) 
 { 
     COORD coord;
@@ -25,39 +26,42 @@ void gotoXY (int column, int line)
     coord.Y = line;
     SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE),coord);
 }
- //Функция инициализации игрового поля и змейки
+
+ //Function of initialization of the game field and snake
 void init_field () 
     { 
-        // инициализируем игровое поле
+        // Initialize the game field
         for (int i = 0; i <= height; ++i) 
             for (int j = 0; j <= width; ++j)
-                field[i][j] = (i == 0 || j == 0 || i == height || j == width ? '#' : ' '); //определяем границы
-        // инициализируем структуру змейки
+                field[i][j] = (i == 0 || j == 0 || i == height || j == width ? '#' : ' '); //Define the borders
+        // Initialize the structure of the snake
         size = 2;
         co_x[0] = 1;
         co_y[0] = 1;
         co_x[1] = 2;
         co_y[1] = 1;
-		//"Тело" змейки, состоящее из символов 'O'
+		//"Snake body" consisting of symbols 'O'
         field[1][1] = 'O';
         field[1][2] = 'O';
     }
- // Функция генерации фрукта на поле
+	
+ // Function of generating food on the field
 void generate_food () 
     {  
 	do{
-		srand( time(0) );//Рандом на каждом запуске разный
+		srand( time(0) );//Random on each launch is different.
         foody = (rand() % 15)+1;
         foodx = (rand() % 45)+1;
-	}while(field[foody][foodx]=='O'); //Защита от появления фрукта на змейке
+	}while(field[foody][foodx]=='O'); //Protection from the appearance of food on the snake
         gotoXY(foodx, foody);
-        cout << "*"; //Отрисовка фрукта символом '*'
+        cout << "*"; //Drawing the food by symbol '*'
     }
- //Функция отрисовки поля
+	
+ //Function of drawing a field
 void draw_field () 
     {
         int i,j;
-        system("cls"); //очищаем экран
+        system("cls"); //Clear screen
         for (i = 0; i <= height; ++i)
             {
                 for (j = 0; j <= width; ++j)
@@ -65,10 +69,11 @@ void draw_field ()
                 cout << "\n";   
             }
         cout << "\n";
-        // инициализируем фрукт
+        // Initialize the food
         generate_food();
     }
-//Функция помещения фрукта на поле
+	
+//Function of placing fruit on the field
 bool isFoodField ()
     {
         if (co_x[size]==foodx && co_y[size]==foody) 
@@ -80,11 +85,11 @@ bool isFoodField ()
         }
         return false;
     }
-//Функция проверки, съела ли змейка саму себя
+//The function of checking whether the snake ate itself
 bool dead () 
     { 
         int i;
-        // врезалась в границы поля?
+        // Crashed into the borders of the field?
         if (co_x[size-1] == width) 
             return true;
         if (co_x[size-1] == 0) 
@@ -93,13 +98,14 @@ bool dead ()
             return true;
         if (co_y[size-1] == 0) 
             return true;
-        // попала сама в себя?
+        // Hit itself in itself?
         for (i = 0; i < size-3; ++i)
             if (co_x[i] == co_x[size-1] && co_y[i] == co_y[size-1])
                return true;
         return false;
     }
-//Функция с основными операциями при движении
+	
+//Function with the basic operations at movement
 void move () 
     { 
         char input;
@@ -107,32 +113,32 @@ void move ()
         int speed=300;
         char ending[]="GAME OVER!";
         do {
-            if (kbhit()) input = getch(); //проверяем нажатие клавиши на консоли 
+            if (kbhit()) input = getch(); //Check the key press on the console
             else input = 0;
-            switch (input)//глобальное значение клавиши и игнорируем движение обратно
+            switch (input)//The global value of the key and ignore the movement back
             {
                 case 'P': 
-                    if (direction==DIR_UP) //  если пробуем повернуть наверх, то ничего не получается
-                        break;// операция прерывается
-                    temp_dir=DIR_DOWN; // можно только вниз
+                    if (direction==DIR_UP) //  If we try to turn up, nothing happens
+                        break;// The operation is interrupt
+                    temp_dir=DIR_DOWN; // Can only move down
                     break;
-                case 'H':  //Аналогично для других сторон
-                    if (direction==DIR_DOWN) break; //Движение вниз
+                case 'H':  //Similarly for other sides.
+                    if (direction==DIR_DOWN) break; //Move down
                     temp_dir=DIR_UP;
                     break;
                 case 'K':
-                    if (direction==DIR_RIGHT) break; //Движение влево
+                    if (direction==DIR_RIGHT) break; //Move left
                     temp_dir= DIR_LEFT; 
                     break;
                 case 'M': 
-                    if (direction==DIR_LEFT) break; //Движение вправо
+                    if (direction==DIR_LEFT) break; //Move right
                     temp_dir= DIR_RIGHT; 
                     break;
                 default:
                     break;
             }
             direction=temp_dir;
-            switch (temp_dir) // движение от головы змейки по указанному направлению
+            switch (temp_dir) // Movement from the head of the snake in the specified direction
             {
                 case DIR_RIGHT:
                     co_x[size] = co_x[size-1]+1;
@@ -153,8 +159,8 @@ void move ()
                 default:
                     break;
             }
-            // если фрукт на поле и змейка его съела, то растёт сама и появляется новый фрукт,
-			// скорость увеличивается
+            // If the fruit on the field and the snake ate it, it grows itself
+			// and a new fruit appears, the speed increases.
             if (isFoodField()) 
                 {
                     gotoXY (co_x[size], co_y[size]);
@@ -176,21 +182,21 @@ void move ()
                 gotoXY (co_x[size], co_y[size]);
                 cout << "O";
             }
-            // если змейка умерла, то конец игры
+            // If the snake died, then the end of the game
             if(dead()) 
                 {
                 for (int i=0; i<11; i++) 
                     {
                         gotoXY (i+18, 7);
-                        cout << ending[i]; // Вывод надписи "Game over"
+                        cout << ending[i]; // The conclusion of the inscription "Game over"
                     }
             break;
                 }
-            // обозначаем скорость и выводим текущий счет
+            // Denote the speed and output the current score
             gotoXY(1,18);
             cout <<"Score : "<< score <<"\n\n";
             Sleep(speed);
-        } while (input!=27); // Выход из игры по нажатию клавиши 'esc'
+        } while (input!=27); // Exit the game by pressing the 'esc' key 
     }
 int main() 
     { 
